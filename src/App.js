44d0b490c7cb73a2c327ae075acea14a217902c7
buildrealone <105898692+buildrealone@users.noreply.github.com>
2022-06-18 12:38:31 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Unity, { UnityContext } from 'react-unity-webgl';
+
+const unityContext = new UnityContext({
+  loaderUrl: "Build/counter-webgl.loader.js",
+  dataUrl: "Build/counter-webgl.data",
+  frameworkUrl: "Build/counter-webgl.framework.js",
+  codeUrl: "Build/counter-webgl.wasm",
+})
+
+function App() {
+  function Test() {
+    unityContext.send("GameManager", "BtnClick");
+  }
+  return (
+    <div className="App">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <button onClick={() => Test()}
+        style={{ display: 'block', marginLeft: "71px" }}>웹사이트에서 기능하는 버튼</button>
+      <Unity 
+      style={{ width: '90%', height: "100%", justifySelf: "center", alignSelf: "center" }}
+      unityContext={unityContext} />
+    </div>
+  );
+}
+
+export default App;
